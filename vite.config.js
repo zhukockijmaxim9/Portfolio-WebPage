9e3e68e6b4e95858fafa9de5ba5db025c54924f8
zhukockijmaxim9 <zhukockijmaxim@gmail.com>
2026-02-23 15:03:42 +0400

    Fix Actions 3 (final)

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Portfolio-WebPage/",
   plugins: [react()],
-  // test: {
-  //   environment: "jsdom",
-  //   globals: true,
-  //   setupFiles: "./src/setupTests.js",
-  // },
 });
