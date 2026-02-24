e48c9ed0809d10677927d6ce4b7c831032be0e01
zhukockijmaxim9 <zhukockijmaxim@gmail.com>
2026-02-24 11:44:11 +0400

    Пуш изменений, чекпоинт

--- src/pages/main/sections/HeroSection.jsx
+++ src/pages/main/sections/HeroSection.jsx
@@ -58,7 +58,7 @@ function HeroSection() {
   return (
     <section className="hero" id="home">
       <div className="hero-pillar-bg" aria-hidden="true">
-        {/* <LightPillar
+        <LightPillar
           topColor="#5227FF"
           bottomColor="#b2ff9f"
           backgroundColor="#000000"
@@ -72,7 +72,7 @@ function HeroSection() {
           interactive={false}
           mixBlendMode="normal"
           quality="high"
-        /> */}
+        />
       </div>
 
       <Navigation />
